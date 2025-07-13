94998b99c4ddbf540c9190f2cca865840fef437f
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-13 10:20:05 -0500

    Add company request

---
+++ RiskTrackSCF_ManagmentWEB/Models/CreateCompanyRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace RiskTrackSCF_ManagmentWEB.Models
+{
+    public class CreateCompanyRequest
+    {
+        public string? Name { get; set; }
+        public string? RUC { get; set; }
+        public string? Sector { get; set; }
+
+
+    }
+}
