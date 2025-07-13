7474658c988a590e7fd91dd0466d6978275cd159
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-13 09:27:05 -0500

    Add User model

---
+++ RiskTrackSCF_ManagmentWEB/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace RiskTrackSCF_ManagmentWEB.Models
+{
+    public class User
+    {
+        public class UserDto
+        {
+            public int UserId { get; set; }
+            public int? CompanyId { get; set; }
+            public string? Username { get; set; }
+            public string? Email { get; set; }
+            public string? Password { get; set; }
+            public string Role { get; set; } = "A";
+        }
+    }
+}
