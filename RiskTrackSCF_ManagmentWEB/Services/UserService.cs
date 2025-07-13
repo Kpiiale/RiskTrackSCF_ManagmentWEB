f1cfd97c12c002797bd30d491ef4b392939a2ed8
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-13 10:22:35 -0500

    Fix User service

--- RiskTrackSCF_ManagmentWEB/Services/UserService.cs
+++ RiskTrackSCF_ManagmentWEB/Services/UserService.cs
@@ -1,4 +1,4 @@
-﻿using static RiskTrackSCF_ManagmentWEB.Models.User;
+﻿using RiskTrackSCF_ManagmentWEB.Models;
 
 namespace RiskTrackSCF_ManagmentWEB.Services
 {
@@ -11,23 +11,23 @@ namespace RiskTrackSCF_ManagmentWEB.Services
             _http = http;
         }
 
-        public async Task<List<UserDto>?> GetUsersAsync()
+        public async Task<List<User>?> GetUsersAsync()
         {
-            return await _http.GetFromJsonAsync<List<UserDto>>("api/Users");
+            return await _http.GetFromJsonAsync<List<User>>("api/Users");
         }
 
-        public async Task<UserDto?> GetUserByIdAsync(int id)
+        public async Task<User?> GetUserByIdAsync(int id)
         {
-            return await _http.GetFromJsonAsync<UserDto>($"api/Users/{id}");
+            return await _http.GetFromJsonAsync<User>($"api/Users/{id}");
         }
 
-        public async Task<bool> CreateUserAsync(UserDto user)
+        public async Task<bool> CreateUserAsync(User CreateUserRequest)
         {
-            var response = await _http.PostAsJsonAsync("api/Users", user);
+            var response = await _http.PostAsJsonAsync("api/Users", CreateUserRequest);
             return response.IsSuccessStatusCode;
         }
 
-        public async Task<bool> UpdateUserAsync(int id, UserDto user)
+        public async Task<bool> UpdateUserAsync(int id, User user)
         {
             var response = await _http.PutAsJsonAsync($"api/Users/{id}", user);
             return response.IsSuccessStatusCode;
