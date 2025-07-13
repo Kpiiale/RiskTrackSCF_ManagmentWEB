b745470fa4a7d2ad7e18bae62add8eec9c17f4e7
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-13 09:57:38 -0500

    Add UserLoginDTO

---
+++ RiskTrackSCF_ManagmentWEB/Models/UserLoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace RiskTrackSCF_ManagmentWEB.Models
+{
+    public class UserLoginDTO
+    {
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [JsonPropertyName("Email")]
+        public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "El campo Email es obligatorio.")]
+        [JsonPropertyName("Password")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
