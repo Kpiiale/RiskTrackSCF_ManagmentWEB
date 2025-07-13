2ffa441cf9ddd90ede98676b3a683500e950a3e3
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-13 09:22:19 -0500

    Add Company Model

---
+++ RiskTrackSCF_ManagmentWEB/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace RiskTrackSCF_ManagmentWEB.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Ruc { get; set; } = string.Empty;
+        public string Sector { get; set; } = string.Empty;
+    }
+}
